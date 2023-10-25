455e29a18cb35bcbd0170eecc9e4582a9da5308d
AmbroseNTK <kiettuannguyense@gmail.com>
2023-10-25 11:22:07 +0700

    Sign up with default role id

--- internal/auth/usecase/base.go
+++ internal/auth/usecase/base.go
@@ -166,10 +166,7 @@ func (a *AuthUseCase) SignUp(ctx context.Context, auth *domain.Auth) error {
 	}
 	auth.Hpassword = string(hashedPassword)
 
-	// set default role id
-	if auth.RoleId == "" {
-		auth.RoleId = a.defaultRoleId
-	}
+	auth.RoleId = a.defaultRoleId
 
 	// create new auth
 	err = a.repo.Create(ctx, auth)
