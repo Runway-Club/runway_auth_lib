8022abcd2047fa19878d482ec0d9027e336760be
AmbroseNTK <kiettuannguyense@gmail.com>
2023-12-05 15:01:17 +0700

    expose auth repo

--- auth.go
+++ auth.go
@@ -106,3 +106,7 @@ func CheckAuthWithProvider(ctx context.Context, token string) (bool, error) {
 func VerifyToken(ctx context.Context, token string) (auth *domain.Auth, err error) {
 	return authUseCase.Verify(ctx, token)
 }
+
+func GetAuthRepo() domain.AuthRepository {
+	return authRepo
+}
