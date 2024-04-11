48bc0affc86fe016ad88f46b9ea8629618af5d69
AmbroseNTK <kiettuannguyense@gmail.com>
2024-04-11 20:18:28 +0700

    add second chance for verifying token

--- internal/auth/usecase/base.go
+++ internal/auth/usecase/base.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Runway-Club/auth_lib/common"
 	"github.com/Runway-Club/auth_lib/domain"
 	"github.com/Runway-Club/auth_lib/utils"
+	jwtlib "github.com/golang-jwt/jwt/v5"
 	"github.com/spf13/viper"
 	"golang.org/x/crypto/bcrypt"
 	"log"
@@ -18,6 +19,7 @@ type AuthUseCase struct {
 	hashCost       string
 	jwt            domain.JwtGenerator
 	defaultRoleId  string
+	projectId      string
 }
 
 func (a *AuthUseCase) GetStaticUserList(ctx context.Context) (list *domain.StaticUserList, err error) {
@@ -136,6 +138,62 @@ func (a *AuthUseCase) CheckAuthWithProvider(ctx context.Context, provider domain
 	return true, nil
 }
 
+func (a *AuthUseCase) customVerifyToken(ctx context.Context, token string) (uid string, claims map[string]interface{}, err error) {
+
+	parsedToken, _ := jwtlib.Parse(token, func(token *jwtlib.Token) (interface{}, error) {
+		return []byte("AllYourBase"), nil
+	})
+	claims, ok := parsedToken.Claims.(jwtlib.MapClaims)
+	if !ok {
+		return "", nil, domain.ErrInvalidToken
+	}
+	// check expiration in seconds
+	exp, ok := claims["exp"].(int64)
+	if !ok {
+		return "", nil, domain.ErrInvalidToken
+	}
+	if time.Now().Unix() > exp {
+		return "", nil, domain.ErrExpiredToken
+
+	}
+	// check issuer
+	issuer, ok := claims["iss"].(string)
+	if !ok {
+		return "", nil, domain.ErrInvalidToken
+	}
+	// Must be "https://securetoken.google.com/<projectId>", where <projectId> is the same project ID used for aud above.
+	if issuer != fmt.Sprintf("https://securetoken.google.com/%s", a.projectId) {
+		return "", nil, domain.ErrInvalidIssuer
+	}
+
+	uid, ok = claims["sub"].(string)
+	if !ok {
+		return "", nil, domain.ErrInvalidToken
+	}
+	// get name
+	name, ok := claims["name"].(string)
+	if !ok {
+		return "", nil, domain.ErrInvalidToken
+	}
+	// get email
+	email, ok := claims["email"].(string)
+	if !ok {
+		return "", nil, domain.ErrInvalidToken
+	}
+	// get picture
+	picture, ok := claims["picture"].(string)
+	if !ok {
+		return "", nil, domain.ErrInvalidToken
+	}
+	return uid, map[string]interface{}{
+		"name":    name,
+		"email":   email,
+		"picture": picture,
+		"user_id": uid,
+	}, nil
+
+}
+
 func (a *AuthUseCase) SignUpWithProvider(ctx context.Context, provider domain.Provider, token string) error {
 	// bearer process
 	if len(token) > 7 && token[:7] == "Bearer " {
@@ -143,6 +201,11 @@ func (a *AuthUseCase) SignUpWithProvider(ctx context.Context, provider domain.Pr
 	}
 	uid, _, err := provider.VerifyToken(ctx, token)
 	if err != nil {
+		// second chance for custom verify token
+		uid, _, err = a.customVerifyToken(ctx, token)
+		if err != nil {
+			return err
+		}
 		return err
 	}
 	// look for existing username
@@ -170,6 +233,8 @@ func (a *AuthUseCase) SignInWithProvider(ctx context.Context, provider domain.Pr
 	}
 	uid, claims, err := provider.VerifyToken(ctx, token)
 	if err != nil {
+		// second chance for custom verify token
+		uid, claims, err = a.customVerifyToken(ctx, token)
 		return nil, err
 	}
 	// get by username
@@ -269,6 +334,7 @@ func NewAuthUseCase(repo domain.AuthRepository, jwt domain.JwtGenerator) *AuthUs
 		passwordPolicy: viper.GetString("runway_auth.password.policy"),
 		hashCost:       viper.GetString("runway_auth.password.cost"),
 		defaultRoleId:  viper.GetString("runway_auth.default_role_id"),
+		projectId:      viper.GetString("runway_auth.projectid"),
 		jwt:            jwt,
 	}
 	// init static users, omit error because it's okay if it's already exist
